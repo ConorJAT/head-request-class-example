9033ae5a36974571da2e10c1be462f0bc024ba6e
Conor Race (RIT Student) <ctr9664@rit.edu>
2024-02-05 14:50:50 -0500

    Worked on head requests in-class demo.

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -6,39 +6,54 @@ const respondJSON = (request, response, status, object) => {
   const headers = {
     'Content-Type': 'application/json',
   };
-  
+
   response.writeHead(status, headers);
   response.write(JSON.stringify(object));
   response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
 
+  response.writeHead(status, headers);
+  response.end();
 };
 
 const getUsers = (request, response) => {
+  const responseJSON = {
+    users
+  };
 
+  return respondJSON(request, response, 200, responseJSON)
 };
 
 const getUsersMeta = (request, response) => {
-
+  return respondJSONMeta(request, response, 200);
 };
 
 const updateUser = (request, response) => {
+  const newUser = {
+    createDate: Date.now()
+  };
 
+  users[newUser.createDate] = newUser;
+
+  return respondJSON(request, response, 201, {newUser});
 };
 
 const notFound = (request, response) => {
   const responseJSON = {
     message: 'The page you are looking for was not found.',
     id: 'notFound',
-  }
+  };
 
   return respondJSON(request, response, 404, responseJSON);
 };
 
 const notFoundMeta = (request, response) => {
-
+  return respondJSONMeta(request, response, 404);
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -6,20 +6,42 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  '/': htmlHandler.getIndex,
-  notFound: jsonHandler.notFound,
-}
+  'GET': {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/updateUsers': jsonHandler.updateUser,
+    notFound: jsonHandler.notFound
+  },
+  'HEAD': {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFoundMeta
+  }
+};
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
-  if(urlStruct[parsedUrl.pathname]){
-    urlStruct[parsedUrl.pathname](request, response);
+  // methodHandlers is either the 'GET' or 'HEAD' object from urlStruct.
+  const methodHandlers = urlStruct[request.method];
+  if (!methodHandlers) {
+    return urlStruct['HEAD'].notFound(request, response);
+  }
+
+  // handlerFunction is specific function based off the object methodHandlers represents.
+  const handlerFunction = methodHandlers[parsedUrl.pathname];
+  if(handlerFunction) {
+    handlerFunction(request, response);
   } else {
-    urlStruct.notFound(request, response);
+    methodHandlers[parsedUrl.pathname].notFound(request, response);
   }
 
 
+  // if (urlStruct[parsedUrl.pathname]) {
+  //   urlStruct[parsedUrl.pathname](request, response);
+  // } else {
+  //   urlStruct.notFound(request, response);
+  // }
 };
 
 http.createServer(onRequest).listen(port, () => {
